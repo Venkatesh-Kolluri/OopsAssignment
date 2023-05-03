32866e7c01f58a701bcf30cd1f44945bf7d84f90
venky <venky94.vk@gmail.com>
2023-05-03 17:06:22 +0530

    [Venkatesh] Added DeckofCardsExtended File added Successfully

---
+++ DeckOfCardsExtended/Player.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCardsExtended
+{
+
+    public class Player
+    {
+        private Node head;
+        private int count;
+        private class Node
+        {
+            public string data;
+            public Node next;
+
+            public Node(string data)
+            {
+                this.data = data;
+                next = null;
+            }
+        }
+        public Player()
+        {
+            head = null;
+            count = 0;
+        }
+        public void AddCard(string card)
+        {
+            Node newNode = new Node(card);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+            count++;
+        }
+        public void SortCards()
+        {
+            for (Node i = head; i != null; i = i.next)
+            {
+                Node min = i;
+                for (Node j = i.next; j != null; j = j.next)
+                {
+                    {
+                        min = j;
+                    }
+                }
+                string temp = i.data;
+                i.data = min.data;
+                min.data = temp;
+            }
+        }
+
+        public void PrintCards()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data);
+                current = current.next;
+            }
+        }
+    }
+
+}
+
---
+++ DeckOfCardsExtended/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+
+namespace DeckOfCardsExtended
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+            string[] Rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+            string[] Deck = new string[52];
+
+            for (int i = 0; i < Rank.Length; i++)
+            {
+                for (int j = 0; j < Suits.Length; j++)
+                {
+                    Deck[Suits.Length * i + j] = Rank[i] + " of " + Suits[j];
+                }
+            }
+
+            Random rand = new Random(); //shuffel
+            for (int i = Deck.Length - 1; i >= 0; i--)
+            {
+                int j = rand.Next(i + 1);
+                string temp = Deck[j];
+                Deck[j] = Deck[i];
+                Deck[i] = temp;
+            }
+
+            Player[] player = new Player[4];  //distribute
+            for (int i = 0; i < 4; i++)
+            {
+                player[i] = new Player();
+                for (int j = 0; j < 9; j++)
+                {
+                    player[i].AddCard(Deck[i * 9 + j]);
+                }
+                player[i].SortCards();
+            }
+
+            for (int i = 0; i < 4; i++) //players with cards
+            {
+                Console.WriteLine($"Player {i + 1} cards:");
+                player[i].PrintCards();
+                Console.WriteLine("==============================================================================");
+            }
+        }
+    }
+}
