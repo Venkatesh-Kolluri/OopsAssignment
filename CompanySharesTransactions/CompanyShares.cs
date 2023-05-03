64f505422ff284b2a07c774578aad3d92bb66edc
venky <venky94.vk@gmail.com>
2023-05-03 17:00:57 +0530

    [Venkatesh] Added CompanyTransaction File Successfully

---
+++ CompanySharesTransactions/CompanyShares.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyShareSymbol
+{
+    class CompanyShares
+    {
+        public string CompanyName;
+        public string Symbol;
+        public int NumberOfShares;
+        public DateTime TransactionDateTime;
+
+        public CompanyShares(string companyName, string symbol,int numOfShares,DateTime dateTime)
+        {
+            CompanyName = companyName;
+            Symbol = symbol;
+            NumberOfShares = numOfShares;
+            TransactionDateTime = dateTime;
+
+        }
+    }
+
+    class LinkedListNode
+    {
+        public CompanyShares Value;
+        public LinkedListNode Next;
+
+        public LinkedListNode(CompanyShares value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    class LinkedList
+    {
+        public LinkedListNode head;
+        private LinkedListNode tail;
+
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+        public void Add(CompanyShares value)
+        {
+            LinkedListNode newNode = new LinkedListNode(value);
+
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+        }
+
+        public void Remove(LinkedListNode nodeToRemove)
+        {
+            LinkedListNode currentNode = head;
+            LinkedListNode previousNode = null;
+
+            while (currentNode != null)
+            {
+                if (currentNode == nodeToRemove)
+                {
+                    if (previousNode == null)
+                    {
+                        head = currentNode.Next;
+                    }
+                    else
+                    {
+                        previousNode.Next = currentNode.Next;
+                    }
+
+                    if (currentNode.Next == null)
+                    {
+                        tail = previousNode;
+                    }
+
+                    break;
+                }
+
+                previousNode = currentNode;
+                currentNode = currentNode.Next;
+            }
+        }
+
+        public void Print()
+        {
+            LinkedListNode currentNode = head;
+
+            while (currentNode != null)
+            {
+                Console.WriteLine("CompanyName: {0},Symbol: {1}, Number of Shares: {2},TransactionDateTime: {3}",
+                    currentNode.Value.CompanyName,currentNode.Value.Symbol, currentNode.Value.NumberOfShares, currentNode.Value.TransactionDateTime);
+                currentNode = currentNode.Next;
+            }
+        }
+    }
+}
---
+++ CompanySharesTransactions/Program.cs
@@ -0,0 +1,27 @@
+﻿using CompanyShareSymbol;
+
+namespace CompanySharesTransactions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.Add(new CompanyShares("Tata Consultancy Services", "TCS", 100, DateTime.Now));
+            linkedList.Add(new CompanyShares("INDIAN ENERGY EXCHANGE", "IEX", 50, DateTime.Now));
+            linkedList.Add(new CompanyShares("TATA POWER PLANT", "TATAPOWER", 10, DateTime.Now));
+
+
+            linkedList.Print();
+
+            LinkedListNode remove = linkedList.head.Next;
+            linkedList.Remove(remove);
+
+            linkedList.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
